6cdfdb4ec5465990baa1d8114e4b72dd68170f3b
deltanedas <@deltanedas:kde.org>
2026-01-20 10:24:10 +0000

    fix lint, mostly cleanup job icons

--- Content.Server/Destructible/Thresholds/Behaviors/GibBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/GibBehavior.cs
@@ -11,12 +11,26 @@ namespace Content.Server.Destructible.Thresholds.Behaviors
     public sealed partial class GibBehavior : IThresholdBehavior
     {
         [DataField("recursive")] private bool _recursive = true;
+        /// <summary>
+        /// Trauma - delete the gibs afterwards if true.
+        /// </summary>
+        [DataField]
+        public bool DeleteGibs;
 
         public LogImpact Impact => LogImpact.Extreme;
 
         public void Execute(EntityUid owner, SharedDestructibleSystem system, EntityUid? cause = null)
         {
-            system.Gibbing.Gib(owner, _recursive);
+            // <Trauma> - store gibs for deletion if enabled
+            var gibs = system.Gibbing.Gib(owner, _recursive);
+            if (DeleteGibs)
+            {
+                foreach (var gib in gibs)
+                {
+                    system.EntityManager.QueueDeleteEntity(gib);
+                }
+            }
+            // </Trauma>
         }
     }
 }
